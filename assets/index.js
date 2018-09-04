9d931f80d67e8014eb1575a3205d18651fc76e52
VamOSGS <gegham.samvelyan@hotmail.com>
2018-09-04 16:16:07 +0500

    Updates

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+import './express.png';
+import './koa.png';
+import './react.png';
+import './redux.png';
+import './mongo.png';
+import './node.png';
+import './webpack.png';
+import './docker.png';
+import './graphql.png';
+import './vue.png';
+import './flow.svg';
+import './less.svg';
+import './sass.svg';
+import './favicon.ico';
